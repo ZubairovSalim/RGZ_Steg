eda0704a964a954b9d71ce4dac5cb27697c0a919
Salim Zubairov <zubairov.salim@yandex.ru>
2017-05-09 02:03:55 +0500

    added number of inputed symbols

---
+++ RGZ1/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace RGZ1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_Share = new System.Windows.Forms.Button();
+            this.btn_Encrypt = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Number = new System.Windows.Forms.TextBox();
+            this.txt_Decrypt = new System.Windows.Forms.TextBox();
+            this.btn_Decrypt = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.txt_Encrypt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Share
+            // 
+            this.btn_Share.Location = new System.Drawing.Point(9, 205);
+            this.btn_Share.Name = "btn_Share";
+            this.btn_Share.Size = new System.Drawing.Size(45, 42);
+            this.btn_Share.TabIndex = 0;
+            this.btn_Share.Text = "Share";
+            this.btn_Share.UseVisualStyleBackColor = true;
+            this.btn_Share.Click += new System.EventHandler(this.btn_Share_Click);
+            // 
+            // btn_Encrypt
+            // 
+            this.btn_Encrypt.Location = new System.Drawing.Point(111, 205);
+            this.btn_Encrypt.Name = "btn_Encrypt";
+            this.btn_Encrypt.Size = new System.Drawing.Size(51, 42);
+            this.btn_Encrypt.TabIndex = 4;
+            this.btn_Encrypt.Text = "Encrypt";
+            this.btn_Encrypt.UseVisualStyleBackColor = true;
+            this.btn_Encrypt.Click += new System.EventHandler(this.btn_Encrypt_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(25, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 163);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btn_Reset);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_Number);
+            this.groupBox1.Controls.Add(this.txt_Decrypt);
+            this.groupBox1.Controls.Add(this.btn_Decrypt);
+            this.groupBox1.Controls.Add(this.btn_Save);
+            this.groupBox1.Controls.Add(this.txt_Encrypt);
+            this.groupBox1.Controls.Add(this.btn_Encrypt);
+            this.groupBox1.Controls.Add(this.btn_Share);
+            this.groupBox1.Location = new System.Drawing.Point(11, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 257);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control";
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Location = new System.Drawing.Point(223, 205);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(52, 42);
+            this.btn_Reset.TabIndex = 16;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Hidden message:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Enter number of symbols:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Enter a hidding message:";
+            // 
+            // txt_Number
+            // 
+            this.txt_Number.Location = new System.Drawing.Point(9, 105);
+            this.txt_Number.Multiline = true;
+            this.txt_Number.Name = "txt_Number";
+            this.txt_Number.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_Number.Size = new System.Drawing.Size(121, 25);
+            this.txt_Number.TabIndex = 10;
+            this.txt_Number.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_Number_KeyDown);
+            this.txt_Number.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Number_KeyPress);
+            // 
+            // txt_Decrypt
+            // 
+            this.txt_Decrypt.Location = new System.Drawing.Point(9, 149);
+            this.txt_Decrypt.Multiline = true;
+            this.txt_Decrypt.Name = "txt_Decrypt";
+            this.txt_Decrypt.ReadOnly = true;
+            this.txt_Decrypt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_Decrypt.Size = new System.Drawing.Size(266, 50);
+            this.txt_Decrypt.TabIndex = 9;
+            // 
+            // btn_Decrypt
+            // 
+            this.btn_Decrypt.Location = new System.Drawing.Point(168, 205);
+            this.btn_Decrypt.Name = "btn_Decrypt";
+            this.btn_Decrypt.Size = new System.Drawing.Size(52, 42);
+            this.btn_Decrypt.TabIndex = 7;
+            this.btn_Decrypt.Text = "Decrypt";
+            this.btn_Decrypt.UseVisualStyleBackColor = true;
+            this.btn_Decrypt.Click += new System.EventHandler(this.btn_Decrypt_Click);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(60, 205);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(45, 42);
+            this.btn_Save.TabIndex = 6;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // txt_Encrypt
+            // 
+            this.txt_Encrypt.Location = new System.Drawing.Point(9, 36);
+            this.txt_Encrypt.Multiline = true;
+            this.txt_Encrypt.Name = "txt_Encrypt";
+            this.txt_Encrypt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_Encrypt.Size = new System.Drawing.Size(266, 50);
+            this.txt_Encrypt.TabIndex = 5;
+            this.txt_Encrypt.TextChanged += new System.EventHandler(this.txt_Encrypt_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Please wait...";
+            this.label4.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar1.Location = new System.Drawing.Point(25, 205);
+            this.progressBar1.MarqueeAnimationSpeed = 30;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(282, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 14;
+            this.progressBar1.Visible = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Location = new System.Drawing.Point(301, 14);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(330, 257);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Results";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(240, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(644, 287);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Steganography";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Share;
+        private System.Windows.Forms.Button btn_Encrypt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_Encrypt;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Decrypt;
+        private System.Windows.Forms.TextBox txt_Decrypt;
+        private System.Windows.Forms.TextBox txt_Number;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RGZ1/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace RGZ1
+{
+    public partial class Form1 : Form
+    {
+        string filter = "Image Files (*.jpg)|*.jpg|Image Files (.bmp)|*.bmp";
+        string adress = String.Empty;
+        Bitmap resultImage;
+        DCT transforms = new DCT();
+        string message;
+        int mes_length;
+        byte[,] mas_byte;
+        List<byte[,]> mas_sepparated;
+        List<double[,]> mas_dct;
+        List<double[,]> mas_infiltrated;
+        List<double[,]> mas_idct;
+        List<byte[,]> mas_new_sepparated;
+        byte[,] mas_new_byte;
+
+
+        byte[,] mas2_byte;
+        List<byte[,]> mas2_sepparated;
+        List<double[,]> mas2_dct;
+
+        List<Thread> threads = new List<Thread>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            btn_Encrypt.Enabled = false;
+            btn_Decrypt.Enabled = false;
+            btn_Save.Enabled = false;
+        }
+
+        public void DoEncrypt()
+        {
+            try
+            {
+                ImageMaker image = new ImageMaker(adress);
+                mas_byte = image.ArrayFilling();
+                mas_sepparated = image.ArraySeparation(mas_byte);
+
+                if (mas_sepparated.Count < message.Length * 8)
+                {
+                    progressBar1.Visible = false;
+                    label4.Visible = false;
+                    throw new ContainerFlaw("The message length is too large for the container");
+                }
+
+                mas_dct = transforms.DisCosTrans(mas_sepparated);
+                mas_infiltrated = transforms.Infiltration(mas_dct, message, 4, 5, 5, 4);
+                mas_idct = transforms.RevDisCosTrans(mas_infiltrated);
+                mas_new_sepparated = transforms.Normalize(mas_idct);
+                mas_new_byte = transforms.Add(mas_new_sepparated, image.Width, image.Height);
+                resultImage = image.Introduction(mas_new_byte);
+                progressBar1.Visible = false;
+                label4.Visible = false;
+                btn_Share.Enabled = true;
+                btn_Encrypt.Enabled = true;
+                btn_Decrypt.Enabled = true;
+                btn_Save.Enabled = true;
+            }
+            catch (ContainerFlaw ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
+
+        public void DoDecrypt()
+        {
+            ImageMaker image1 = new ImageMaker(adress);
+            mes_length = Convert.ToInt32(txt_Number.Text);
+            mas2_byte = image1.ArrayFilling();
+            mas2_sepparated = image1.ArraySeparation(mas2_byte);
+            mas2_dct = transforms.DisCosTrans(mas2_sepparated);
+            string mes = transforms.Extraction(mas2_dct, 4, 5, 5, 4, mes_length);
+            txt_Decrypt.Text = mes;
+            progressBar1.Visible = false;
+            label4.Visible = false;
+            btn_Share.Enabled = true;
+            btn_Save.Enabled = false;
+            btn_Encrypt.Enabled = true;
+            btn_Decrypt.Enabled = true;
+        }
+
+        private void btn_Share_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opn = new OpenFileDialog();
+            opn.Filter = filter;
+            if (opn.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = new Bitmap(opn.FileName);
+                adress = opn.FileName;
+            }
+            if (adress != String.Empty)
+            {
+                btn_Encrypt.Enabled = true;
+                btn_Decrypt.Enabled = true;
+            }
+        }
+
+        private void btn_Encrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txt_Encrypt.Text == String.Empty)
+                {
+                    throw new ContainerFlaw("Please, enter a hidding message");
+                }
+                progressBar1.Visible = true;
+                label4.Visible = true;
+                btn_Share.Enabled = false;
+                btn_Encrypt.Enabled = false;
+                btn_Decrypt.Enabled = false;
+                message = txt_Encrypt.Text;
+                ThreadStart deleg = new ThreadStart(DoEncrypt);
+                Thread thr = new Thread(deleg);
+                threads.Add(thr);
+                thr.Start();
+            }
+            catch(ContainerFlaw ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+          
+        }
+
+        private void btn_Decrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(txt_Number.Text == String.Empty)
+                {
+                    throw new ContainerFlaw("Please, enter number of symbols");
+                }
+
+                progressBar1.Visible = true;
+                label4.Visible = true;
+                btn_Share.Enabled = false;
+                btn_Save.Enabled = false;
+                btn_Encrypt.Enabled = false;
+                btn_Decrypt.Enabled = false;
+                ThreadStart deleg1 = new ThreadStart(DoDecrypt);
+                Thread thr1 = new Thread(deleg1);
+                threads.Add(thr1);
+                thr1.Start();
+            }
+            catch(ContainerFlaw ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+          
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+
+            string result_adress;
+            save.DefaultExt = ".jpg";
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                result_adress = save.FileName;
+
+                ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+                //System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+                //System.Drawing.Imaging.Encoder myEncoder2 = System.Drawing.Imaging.Encoder.Compression;
+
+                //EncoderParameters myEncoderParameters = new EncoderParameters(2);
+
+                //EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 100L);
+                //EncoderParameter myEncoderParameter2 = new EncoderParameter(myEncoder2,(long)EncoderValue.CompressionNone);
+
+                //myEncoderParameters.Param[0] = myEncoderParameter;
+                //myEncoderParameters.Param[1] = myEncoderParameter2;
+
+
+                //resultImage.Save(result_adress, jpgEncoder, myEncoderParameters);
+                resultImage.Save(result_adress);
+            }
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            foreach (Thread t in threads)
+            {
+                t.Abort();
+            }
+
+            progressBar1.Visible = false;
+            btn_Encrypt.Enabled = false;
+            btn_Decrypt.Enabled = false;
+            btn_Save.Enabled = false;
+            pictureBox1.Image = null;
+            txt_Encrypt.Text = String.Empty;
+            txt_Number.Text = String.Empty;
+            txt_Decrypt.Text = String.Empty;
+            label4.Hide();
+        }
+
+        public class ContainerFlaw : Exception//исключение
+        {
+            public ContainerFlaw(string message) : base(message)
+            {
+
+            }
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        bool non = false;
+
+        private void txt_Number_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                char symb = (char)e.KeyData;
+                if (e.KeyCode == Keys.OemMinus)
+                    non = true;
+                if (char.IsNumber(symb) == false)
+                    non = true;
+                if (e.KeyCode == Keys.Back)
+                    non = false;
+                if (symb == '0')
+                {
+                    throw new ContainerFlaw("Length of encrypted message is too small");
+                }
+            }
+            catch(ContainerFlaw ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+
+        private void txt_Number_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (non == true)
+            {
+                e.Handled = true;
+                non = false;
+            }
+
+        }
+
+        private void txt_Encrypt_TextChanged(object sender, EventArgs e)
+        {
+            label5.Text = txt_Encrypt.Text.Length.ToString();
+        }
+    }
+}
