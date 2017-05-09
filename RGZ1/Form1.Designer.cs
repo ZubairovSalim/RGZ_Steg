8d08b3490ed9d7b71b4b1196e7c189637c7ff105
Salim Zubairov <zubairov.salim@yandex.ru>
2017-05-09 17:38:50 +0500

    Changes with label's orientation

--- RGZ1/Form1.Designer.cs
+++ RGZ1/Form1.Designer.cs
@@ -224,7 +224,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(240, 16);
+            this.label5.Location = new System.Drawing.Point(220, 16);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(0, 13);
             this.label5.TabIndex = 17;
